a03d236e8ae01b24b7e951efd8a87ea165a47296
brunoclaumari <brunoclaumari@gmail.com>
2023-03-08 22:53:13 -0300

    Adicionando cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -8,6 +9,7 @@ const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
+server.use(cors);
 server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`JSON Server is running in ${port}`);
