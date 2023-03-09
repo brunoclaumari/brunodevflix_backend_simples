379233717f4ab644f8582790e9c4dfb774f05124
brunoclaumari <brunoclaumari@gmail.com>
2023-03-09 00:02:12 -0300

    Ajuste 14

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
-server.use(cors);
+//server.use(cors);
 server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`JSON Server is running in ${port}`);
